f666f25620f100a30a726fc5eee5d741039ed958
genessamoodie <genessa.moodie@ucdenver.edu>
2016-03-02 22:27:41 -0700

    Finished implementing Clustering.cpp Fixed a few things in Point.cpp Uncommented things in ClusteringTests.cpp, Cluster.h, and main

--- Cluster.h
+++ Cluster.h
@@ -34,10 +34,10 @@ namespace Clustering {
         ~Cluster();
 
         // Getters/setters
-        int getSize() const; // TODO add to the requirements
+        int getSize() const;
 
         // Set functions: They allow calling c1.add(c2.remove(p));
-        void add(const Point &); // TODO add asc order to the requirements
+        void add(const Point &);
         const Point &remove(const Point &);
         bool contains(const Point &);
 
